1cd1d6e6a1484ec2f640e30f585a718bde62cc86
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2024-08-07 11:52:17 +0530

    Update code.py

--- code.py
+++ code.py
@@ -6,3 +6,12 @@ def addMatrices():
             print(" ", result[i][j], end=" ")
         print()
     repeat()
+
+def subMatrices():
+    print("Subtraction of two matrix is:")
+    for i in range(rows):
+        for j in range(columns):
+            result[i][j] = matrix1[i][j] - matrix2[i][j]
+            print(" ", result[i][j], end=" ")
+        print()
+    repeat()
