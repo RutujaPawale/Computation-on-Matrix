e063181cb00aebb6e2c323af5c43eb4a99baa0b2
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2024-08-07 11:52:02 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,8 @@
-
+def addMatrices():
+    print("Addition of two matrix is:")
+    for i in range(rows):
+        for j in range(columns):
+            result[i][j]=matrix1[i][j] + matrix2[i][j]
+            print(" ", result[i][j], end=" ")
+        print()
+    repeat()
